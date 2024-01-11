cd423fe35733d0e57605404598495d5c3838cf55
xiaoxing_xy <74756712+xiaoxing1748@users.noreply.github.com>
2024-01-12 01:15:20 +0800

    添加等待队列功能

--- wechat_api.py
+++ wechat_api.py
@@ -1,5 +1,6 @@
 import logging
 import datetime
+import time
 from flask import Flask
 from flask import request
 from wechatpy.utils import check_signature
@@ -33,77 +34,100 @@ url = config.get_url_path()
 client = WeChatClient(wechat_appid, wechat_appsecret)
 
 
-# 加载embedding
-embedding = embeddings.load()
-# 向量存储
-document_path = "./document/news.txt"
-vector_store = ""
-vector_store = faiss_vector_store.index(document_path, embedding)
+# 初始化向量存储
+def get_vector_store():
+    # 加载embedding
+    embedding = embeddings.load()
+    # 向量存储
+    return faiss_vector_store.index(document_path, embedding)
 
 
-# 回复缓存
-response_dict = {}
+# 搜寻文档
+def get_docs(question):
+    return faiss_vector_store.search(question, document_path, vector_store)
 
 
+# 保存缓存
 def save_responses(msg, response, response_dict, max_entries=10):
     # 将新的键值对添加到字典中
     response_dict[msg.source] = response
-
+    waiting_for_response.pop(msg.source, None)
     # 如果字典中的键值对数量超过了最大限制，删除最早的键值对
     if len(response_dict) > max_entries:
         # 获取字典的所有键并按照插入顺序排序
         sorted_keys = list(response_dict.keys())
-
         # 删除最早的键值对
         del response_dict[sorted_keys[0]]
 
 
+# 文档路径
+document_path = "./document/news.txt"
+# 向量存储
+vector_store = get_vector_store()
+# 回复缓存
+response_dict = {}
+# 等待队列
+waiting_for_response = {}
+
+
 # 生成回答
 def getresponse(msg):
-    docs = faiss_vector_store.search(msg.content, document_path, vector_store)
-    # 本地ChatGLM
-    if msg.content.startswith("/a "):
-        question = msg.content.split("/a ")[1]
-        response = chatglm.chat(question)['response']
-        save_responses(msg, response, response_dict)
-        print(response_dict)
-        return response
-    # 本地QAChain
-    if msg.content.startswith("/b "):
-        question = msg.content.split("/b ")[1]
-        response = knowledge_chain.qa_chain_legacy(question, vector_store)
-        save_responses(msg, response, response_dict)
-        print(response_dict)
-        return response
-    # 千帆ChatGLM
-    if msg.content.startswith("/c "):
-        question = msg.content.split("/c ")[1]
-        response = qianfan.chat(question).text
-        response = json.loads(response)["result"]
-        save_responses(msg, response, response_dict)
-        print(response_dict)
-        return response
-    # 千帆QAChain
-    if msg.content.startswith("/d "):
-        question = msg.content.split("/d ")[1]
-        response = knowledge_chain.qianfan_chain(question, docs).text
-        response = json.loads(response)["result"]
-        save_responses(msg, response, response_dict)
-        print(response_dict)
-        return response
-    # 千帆知识库
-    if msg.content.startswith("/e "):
-        question = msg.content.split("/e ")[1]
-        response = qianfan.chat_with_knowledge_base(question).text
-        response = json.loads(response)["result"]
-        save_responses(msg, response, response_dict)
-        print(response_dict)
-        return response
+    if msg.source in waiting_for_response:
+        return "正在生成上一次的回答，请稍后再试"
     else:
-        response = "已收到信息"
-        save_responses(msg, response, response_dict)
-        print(response_dict)
-        return response
+        waiting_for_response[msg.source] = True
+        # 本地ChatGLM
+        if msg.content.startswith("/a "):
+            question = msg.content.split("/a ")[1]
+            response = chatglm.chat(question)['response']
+            save_responses(msg, response, response_dict)
+            print(response_dict)
+            return response
+        # 本地QAChain
+        if msg.content.startswith("/b "):
+            question = msg.content.split("/b ")[1]
+            response = knowledge_chain.qa_chain_legacy(question, vector_store)
+            save_responses(msg, response, response_dict)
+            print(response_dict)
+            return response
+        # 千帆ChatGLM
+        if msg.content.startswith("/c "):
+            question = msg.content.split("/c ")[1]
+            response = qianfan.chat(question).text
+            response = json.loads(response)["result"]
+            save_responses(msg, response, response_dict)
+            print(response_dict)
+            return response
+        # 千帆QAChain
+        if msg.content.startswith("/d "):
+            question = msg.content.split("/d ")[1]
+            docs = get_docs(msg.content)
+            response = knowledge_chain.qianfan_chain(question, docs).text
+            response = json.loads(response)["result"]
+            save_responses(msg, response, response_dict)
+            print(response_dict)
+            return response
+        # 千帆知识库
+        if msg.content.startswith("/e "):
+            question = msg.content.split("/e ")[1]
+            response = qianfan.chat_with_knowledge_base(question).text
+            response = json.loads(response)["result"]
+            save_responses(msg, response, response_dict)
+            print(response_dict)
+            return response
+        # 测试
+        if msg.content.startswith("/test "):
+            question = msg.content.split("/test ")[1]
+            time.sleep(10)
+            response = "这是测试内容"
+            save_responses(msg, response, response_dict)
+            print(response_dict)
+            return response
+        else:
+            response = "正在排队中，请稍后发送“/获取回答”"
+            save_responses(msg, response, response_dict)
+            print(response_dict)
+            return response
 
 
 # 已认证公众号的异步回复
@@ -119,7 +143,8 @@ def reply_msg(msg):
     # response = getresponse(msg)
     # 备注：响应回答超时会重试三次，因此响应回答只能用来做获取回答功能，不能用来做聊天功能
     if msg.content == "/获取回答" and msg.source in response_dict:
-        response = f"您最近一次提问的回答是：{response_dict[msg.source]}"
+        # response = f"您最近一次提问的回答是：{response_dict[msg.source]}"
+        response = response_dict[msg.source]
         print("回答:reply:{}".format(response))
         reply = TextReply(content=response, message=msg)
         return reply.render()
